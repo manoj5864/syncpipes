24125643065f619bc9baef97809a9cdec45368a0
Klim Shumaiev <klimksh@gmail.com>
2015-12-03 13:18:24 +0100

    execution under development

--- app/src/components/EAToMysql/EAToMysqlCtrl.js
+++ app/src/components/EAToMysql/EAToMysqlCtrl.js
@@ -7,6 +7,16 @@ function EAToMysqlCtrl($scope, $http, staticService) {
     var uploadUrl = "http://localhost:51248/api/repository/";
     self.EAFile = null;
     self.databases = null;
+    self.tableColumnMap = {};
+    self.objectMapper = [];
+
+    var mysqlConnectionServer = "http://localhost:8084/connectToMysql";
+    var mysqlCreateDB = "http://localhost:8084/createADatabase";
+    var getTables = "http://localhost:8084/getTables";
+    var getColumns = "http://localhost:8084/getColumns";
+    var createTable = "http://localhost:8084/createTable";
+    var insertRow = "http://localhost:8084/insertRow";
+
 
     $scope.showFileUpload = true;
     $scope.showData = false;
@@ -16,7 +26,7 @@ function EAToMysqlCtrl($scope, $http, staticService) {
     };
 
 
-    $scope.getNodes = function () {
+    self.getNodes = function () {
         var data = staticService.dataFromEA();
         var obj = JSON.parse(data);
         var nodes = [];
@@ -28,11 +38,11 @@ function EAToMysqlCtrl($scope, $http, staticService) {
         return nodes;
     };
 
-    $scope.getAttributesOfNode = function(node){
+    self.getAttributesOfNode = function(node){
             var data = staticService.dataFromEA();
             var obj = JSON.parse(data);
             let keys = new Set();
-            console.log(node);
+            
             traverse(obj[node]).forEach(function(){
                 if (this.level == 2){
                     keys.add(this.key);
@@ -45,10 +55,20 @@ function EAToMysqlCtrl($scope, $http, staticService) {
             return keysList;
     };
 
+    self.getAttributesValues = function(node, attribute){
+        var data = staticService.dataFromEA();
+        var obj = JSON.parse(data);
+        var listOfValues = [];
+        for (var i=0;i<obj[node].length;i++){
+            var value = traverse(obj[node]).get([i,attribute]);
+            listOfValues.push(value);
+        }
+        return listOfValues;
+    };
+
     $scope.selectEAFile = function() {
         var fileInput = document.getElementById('EAFile');
-        var file = fileInput.files[0];
-        self.EAFile = file;
+        self.EAFile = fileInput.files[0];
     };
 
     $scope.uploadFile = function(files) {
@@ -91,7 +111,7 @@ function EAToMysqlCtrl($scope, $http, staticService) {
                 if (db.Database === $scope.database) {
                     var options = {};
                     options.databaseName = $scope.database;
-                    var promise = excelToMysqlService.queryMysql(getTables, options);
+                    var promise = staticService.queryMysql(getTables, options);
                     promise.then(
                         function(payload) { self.tables = payload; },
                         function(errorPayload) { alert("Unable to get tables!");}
@@ -107,7 +127,7 @@ function EAToMysqlCtrl($scope, $http, staticService) {
         options.port = $scope.port;
         options.userName = $scope.userName;
         options.password = $scope.password;
-        var promise = excelToMysqlService.queryMysql(mysqlConnectionServer, options);
+        var promise = staticService.queryMysql(mysqlConnectionServer, options);
         promise.then(
             function(payload) { self.databases = payload; },
             function(errorPayload) { alert("Unable to connect to the database!"); }
@@ -117,7 +137,7 @@ function EAToMysqlCtrl($scope, $http, staticService) {
     self.createADatabase = function() {
         var options = {};
         options.databaseName = $scope.databaseName;
-        var promise = excelToMysqlService.queryMysql(mysqlCreateDB, options);
+        var promise = staticService.queryMysql(mysqlCreateDB, options);
         promise.then(function(payload) {
             self.connectToMysql();
             alert("Successfully created a new database!");
@@ -129,7 +149,7 @@ function EAToMysqlCtrl($scope, $http, staticService) {
             var options = {};
             options.databaseName = databaseName;
             options.tableName = tableName;
-            var promise = excelToMysqlService.queryMysql(getColumns, options);
+            var promise = staticService.queryMysql(getColumns, options);
             promise.then(function(payload) {
                 self.tableColumnMap[tableName] = payload; //Field, Type, Null, Key, Default, Extra
                 return payload;
@@ -137,6 +157,121 @@ function EAToMysqlCtrl($scope, $http, staticService) {
         }
     }
 
+    self.createMapping = function(){
+        toggleLink("mappingLink");
+        $("#config").hide();
+        $("#mapper").show();
+
+        for(var i=0; i<self.tables.length; i++)
+            self.getColumns($scope.database, self.tables[i]);
+
+        var nodes = self.getNodes();
+        for (var iNode=0; iNode<nodes.length; iNode++){
+            var map={};
+            map.from = nodes[iNode];
+            var attributes = self.getAttributesOfNode(nodes[iNode])
+            map.attributes=[];
+            for (var iAttribute=0; iAttribute < attributes.length; iAttribute++){
+                var temp = {};
+                temp.from = attributes[iAttribute];
+                map.attributes.push(temp);
+            }
+        }
+        self.objectMapper.push(map);
+        self.nodes = self.getNodes();
+    };
+
+    self.updateObjectMapper = function(node, table) {
+        for(var i=0; i<self.objectMapper.length; i++) {
+            if(self.objectMapper[i].from === node) {
+                self.objectMapper[i].to = table;
+                for(var j=0; j<self.objectMapper[i].attributes.length; j++)
+                    self.objectMapper[i].attributes[j].to = null;
+                // add more logic to update the map automatically here..
+            }
+        }
+    }
+
+    self.updateAttributeMapper = function(node, attribute, tableColumn) {
+        for(var i=0; i<self.objectMapper.length; i++) {
+            if(self.objectMapper[i].from === node) {
+                for(var j=0; j<self.objectMapper[i].attributes.length; j++) {
+                    if(self.objectMapper[i].attributes[j].from === attribute)
+                        self.objectMapper[i].attributes[j].to = tableColumn;
+                }
+                // add more logic to update the map automatically here..
+            }
+        }
+    }
+
+
+    self.executeModel = function() {
+        toggleLink("executionLink");
+        $("#mapper").hide();
+        $("#execution").show();
+        for(var i=0; i<self.objectMapper.length; i++) {
+            var map = self.objectMapper[i];
+
+            //create table
+            if(!self.tableColumnMap.hasOwnProperty(map.to)) {
+                var cols = [];
+                for(var j=0; j<map.attributes.length; j++)
+                    cols.push(map.attributes[j].to);
+                self.createTable($scope.database, map.to, cols);
+            } else {
+                //logic to update table and columns if table exist goes here
+            }
+        }
+
+        sleep(1000);
+        //insert row
+
+        var nodes = self.getNodes();
+        for(var i=0; i<nodes.length; i++) {
+
+            var map = self.getMapFromObjectMapper(self.objectMapper, nodes[i]);
+            
+            console.log(map);
+
+        }
+
+        sleep(1000);
+        $(".log").append("<br /><h3>Execution complete!</h3>");
+    }
+
+    self.getMapFromObjectMapper = function(objectMapper, fromObject) {
+        for(var i=0; i<self.objectMapper.length; i++)
+            if(self.objectMapper[i].from === fromObject) return self.objectMapper[i];
+    }
+
+    self.getExcelRowAsJson = function(sheetName) {
+        for(var i=0; i<self.excelSheets.length; i++)
+            if(self.excelSheets[i].hasOwnProperty(sheetName))
+                return self.excelSheets[i][sheetName];
+        return null;
+    }
+
+    self.createTable = function(databaseName, tableName, cols) {
+        var options = {};
+        options.databaseName = databaseName;
+        options.tableName = tableName;
+        options.cols = cols;
+        var promise = staticService.queryMysql(createTable, options);
+        promise.then(function(payload) {
+            $(".log").append(">> Created table " + tableName + "<br />");
+        }, function(errorPayload) { alert("Unable to create table " + tableName);});
+    }
+
+    self.insertRow = function(databaseName, tableName, attributes) {
+        var options = {};
+        options.databaseName = databaseName;
+        options.tableName = tableName;
+        options.attributes = attributes;
+        var promise = staticService.queryMysql(insertRow, options);
+        promise.then(function(payload) {
+            $(".log").append(">> New row added to " + tableName + "<br />");
+        }, function(errorPayload) { alert("Unable to insert row in table " + tableName + "---" + errorPayload);});
+    }
 
 
 
--- app/src/components/EAToMysql/js/staticService.js
+++ app/src/components/EAToMysql/js/staticService.js
@@ -6,38 +6,10 @@ var staticService = function($http, $q) {
 
     var POST = "POST";
 
-    service.to_json = function(workbook) {
-        var result = {};
-        workbook.SheetNames.forEach(function(sheetName) {
-            var roa = XLS.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-            if(roa.length > 0){
-                result[sheetName] = roa;
-            }
-        });
-        return result;
-    };
-
-    service.connectToMysql = function(options) {
+    service.queryMysql = function(url, options) {
         var deferred = $q.defer();
         $http({
-            url: mysqlConnectionServer,
-            method: POST,
-            async: false,
-            data: options,
-            dataType: "json",
-            contentType: "application/json",
-        }).then(function(res) {
-            deferred.resolve(res.data);
-        }, function(msg) {
-            deferred.reject(msg);
-        });
-        return deferred.promise;
-    };
-
-    service.createADatabase = function(options) {
-        var deferred = $q.defer();
-        $http({
-            url: mysqlCreateDB,
+            url: url,
             method: POST,
             async: false,
             data: options,
